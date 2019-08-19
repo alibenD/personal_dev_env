ff2257a37af6724b41603bcb2f0c58193c1e4682
alibenD <aliben.develop@gmail.com>
2019-08-19 16:49:53 +0800

    Support Coverage Statistics required with GTest

--- vim_template/dynamic/full/CMakeLists.txt.bash
+++ vim_template/dynamic/full/CMakeLists.txt.bash
@@ -5,7 +5,7 @@
 # @name: cmakelists.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2018-01-31 13:47:34
-# @last_modified_date: 2019-08-16 18:48:41
+# @last_modified_date: 2019-08-19 16:39:07
 # @description: TODO
 #---***********************************************---
 
@@ -33,45 +33,89 @@ cmake_minimum_required(VERSION 3.10)
 #project name
 project(${PROJECT_NAME})
 
+# Option
+  OPTION(ENABLE_ALL_WARNING ON)
+  OPTION(ENABLE_WARNING_AS_ERROR OFF)
+  OPTION(ENABLE_WARNING_EXTRA ON)
+  OPTION(ENABLE_VERBOSE ON)
+
+  OPTION(BUILD_GTESTS "Build test example cases with google test" ON)
+  OPTION(WITH_GLOG "Enable google log in this case" OFF)
+
+  OPTION(BUILD_SHARED_LIBS "Build shared libraries" ON)
+
+# Compiler flag
+  IF(ENABLE_ALL_WARNING)
+    SET(CXX_FLAG_ALL_WARNING "-Wall")
+  ENDIF()
+  IF(ENABLE_WARNING_AS_ERROR)
+    SET(CXX_FLAG_WARNING_AS_ERROR "-Werror")
+  ENDIF()
+  IF(ENABLE_WARNING_EXTRA)
+    SET(CXX_FLAG_WARNING_EXTRA "-Wextra")
+  ENDIF()
+  IF(ENABLE_VERBOSE)
+    SET(CMAKE_VERBOSE_MAKEFILE ON)
+  ENDIF()
+
 # CMake Build System Default Setup
-  MESSAGE( STATUS "Project:  ${PROJECT_NAME}" )
+  SET(CMAKE_BUILD_TYPE "Release" CACHE STRING "Default Build Type")
+  SET_PROPERTY(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS Release Debug RelWithDebInfo MinSizeRel)
+
+  #SET(CMAKE_VERBOSE_MAKEFILE OFF)
+  SET(CMAKE_CXX_STANDARD 11)
+  SET(CMAKE_CXX_FLAGS 
+     "\${CMAKE_CXX_FLAGS} \\
+      \${CXX_FLAG_ALL_WARNING} \\
+      \${CXX_FLAG_WARNING_EXTRA} \\
+      \${CXX_FLAG_WARNING_AS_ERROR} \\
+      -std=c++14 \\
+      -fPIC \\
+      -Wno-deprecated-register \\
+      -Wno-deprecated-declarations \\
+      -Wno-unused-private-field \\
+      -Wno-unused-parameter \\
+      -Wno-unused-variable \\
+      -fcolor-diagnostics \\
+      -ftemplate-backtrace-limit=0 \\
+      -march=native \\
+      "
+      )
+  #SET(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
   # Build type default
   IF(NOT CMAKE_BUILD_TYPE)
     SET(CMAKE_BUILD_TYPE RELEASE)
   ENDIF()
+
   IF(CMAKE_BUILD_TYPE STREQUAL "RELEASE")
     ADD_DEFINITIONS(-DNDEBUG)
+    SET(CMAKE_CXX_FLAGS_RELEASE " -O3")
   ELSEIF(CMAKE_BUILD_TYPE STREQUAL "DEBUG")
     SET(CMAKE_DEBUG_POSTFIX _debug)
+    SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g --coverage")
   ENDIF()
 
-  MESSAGE( STATUS "Build Type: " ${CMAKE_BUILD_TYPE})
-  MESSAGE( STATUS "DEBUG POSTFIX: " ${CMAKE_DEBUG_POSTFIX})
-  OPTION(BUILD_GTESTS "Build test example cases with google test" OFF)
-  OPTION(WITH_GLOG "Enable google log in this case" OFF)
-
-  SET(CMAKE_VERBOSE_MAKEFILE OFF)
-  SET(CMAKE_CXX_STANDARD 11)
-  SET(CMAKE_CXX_FLAGS "\${CMAKE_CXX_FLAGS} -Wall -Werror -std=c++14 -fPIC -Wextra -Wno-deprecated-register -Wno-deprecated-declarations -Wno-unused-private-field -Wno-unused-parameter -Wno-unused-variable -Qunused-arguments -fcolor-diagnostics -ftemplate-backtrace-limit=0")
-  #SET(CMAKE_CXX_FLAGS "\${CMAKE_CXX_FLAGS} -Wall -Werror -std=c++14 -Wextra -Wno-deprecated-register -Wno-deprecated-declarations -Qunused-arguments -fcolor-diagnostics -ftemplate-backtrace-limit=0")
-  #SET(CMAKE_POSITION_INDEPENDENT_CODE ON)
+  MESSAGE( STATUS "Project:  \${PROJECT_NAME}" )
+  MESSAGE( STATUS "Build Type: " \${CMAKE_BUILD_TYPE})
+  MESSAGE( STATUS "DEBUG POSTFIX: " \${CMAKE_DEBUG_POSTFIX})
 
   # Cross-platform check
-  IF("\${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    SET(CMAKE_CXX_FLAGS_DEBUG "\${CMAKE_CXX_FLAGS} -O0 -g")
-    SET(CMAKE_CXX_FLAGS_RELEASE "\${CMAKE_CXX_FLAGS} -O3 -march=native")
-  ELSEIF(CMAKE_HOST_APPLE)
-    SET(CMAKE_CXX_FLAGS_RELEASE "\${CMAKE_CXX_FLAGS} -O3 -march=native")
-    SET(CMAKE_CXX_FLAGS_DEBUG "\${CMAKE_CXX_FLAGS} -O0 -g")
-  ELSEIF("\${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
-    SET(CMAKE_CXX_FLAGS_RELEASE "-O3")
-    SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE "\${CMAKE_EXE_LINKER_FLAGS_DEBUG}" --coverage)
-    SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "\${CMAKE_SHARED_LINKER_FLAGS_DEBUG}" --coverage)
-  ELSEIF(CMAKE_CXX_COMPILER_ID MATCHES "^MSVC$")
-    ADD_DEFINITION("-D _USE_MATH_DEFINES /bigobj /wd4305 /wd4244 /MP")
-    ADD_DEFINITION("-D PROJECT_CORE_EXPORTS")
-  ENDIF()
+  #IF("\${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  #  SET(CMAKE_CXX_FLAGS_DEBUG "\${CMAKE_CXX_FLAGS} -O0 -g")
+  #  SET(CMAKE_CXX_FLAGS_RELEASE "\${CMAKE_CXX_FLAGS} -O3")
+  #ELSEIF(CMAKE_HOST_APPLE)
+  #  SET(CMAKE_CXX_FLAGS_RELEASE "\${CMAKE_CXX_FLAGS} -O3")
+  #  SET(CMAKE_CXX_FLAGS_DEBUG "\${CMAKE_CXX_FLAGS} -O0 -g")
+  #ELSEIF("\${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  #  SET(CMAKE_CXX_FLAGS_RELEASE "-O3")
+  #  SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
+  #  SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE "\${CMAKE_EXE_LINKER_FLAGS_DEBUG}" --coverage)
+  #  SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "\${CMAKE_SHARED_LINKER_FLAGS_DEBUG}" --coverage)
+  #ELSEIF(CMAKE_CXX_COMPILER_ID MATCHES "^MSVC$")
+  #  ADD_DEFINITION("-D _USE_MATH_DEFINES /bigobj /wd4305 /wd4244 /MP")
+  #  ADD_DEFINITION("-D PROJECT_CORE_EXPORTS")
+  #ENDIF()
 
   SET(CMAKE_MACOSX_RPATH 1)
   set(CMAKE_SKIP_BUILD_RPATH FALSE)                 # Compile with RPATH
@@ -80,7 +124,6 @@ project(${PROJECT_NAME})
   SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
   SET(EXECUTABLE_OUTPUT_PATH \${PROJECT_SOURCE_DIR}/bin)
   SET(LIBRARY_OUTPUT_PATH \${PROJECT_SOURCE_DIR}/lib)
-  OPTION(BUILD_SHARED_LIBS "Build shared libraries" ON)
   SET(\${PROJECT_NAME}_LIB_TYPE STATIC)
   IF(BUILD_SHARED_LIBS)
     SET(BUILD_SHARED_LIBS SHARED)
@@ -119,9 +162,9 @@ project(${PROJECT_NAME})
   LINK_DIRECTORIES(\${CMAKE_INSTALL_PREFIX}/lib)
   LINK_DIRECTORIES(\${PROJECT_SOURCE_DIR}/lib)
 
-  SET(APPLE_LIBS ${COCOA_LIBRARY} ${IOKit_LIBRARY} ${OpenGL_LIBRARY} ${CoreVideo_LIBRARY})
-  SET(APPLE_LIBS ${APPLE_LIBS} ${GLFW3_LIBRARY} ${ASSIMP_LIBRARY})
-  SET(LIBS ${LIBS} ${APPLE_LIBS})
+  SET(APPLE_LIBS \${COCOA_LIBRARY} \${IOKit_LIBRARY} \${OpenGL_LIBRARY} \${CoreVideo_LIBRARY})
+  SET(APPLE_LIBS \${APPLE_LIBS} \${GLFW3_LIBRARY} \${ASSIMP_LIBRARY})
+  SET(LIBS \${LIBS} \${APPLE_LIBS})
 
   SET( THIRD_PARTY_LIBS
     \${THIRD_PARTY_LIBS}
--- vim_template/dynamic/full/build.bash.bash
+++ vim_template/dynamic/full/build.bash.bash
@@ -4,7 +4,7 @@
 # @name: bashnew.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2017-10-21 17:42:35
-# @last_modified_date: 2019-08-16 18:47:11
+# @last_modified_date: 2019-08-19 16:47:58
 # @brief: Generate a template for new bash file
 #---***********************************************---
 
@@ -32,11 +32,47 @@ cat << EOF
 #---Variables
 CREATED_TIME=\`date '+%Y-%m-%d %H:%M:%S'\`
 CREATED_YEAR=\`date '+%Y'\`
-
+CLEAN=\$1
 #---Shell Command
+if [[ ! -d "build" ]]; then
+  mkdir -p build
+fi
+if [[ "\${CLEAN}" == "clean" ]]; then
+  rm -rf bin/*
+  rm -rf build/*
+  rm -rf lib/*
+fi
+
+if [[ "\${CLEAN}" == "debug" ]]; then
+  BUILD_TYPE=DEBUG
+else
+  BUILD_TYPE=RELEASE
+fi
+
+if [ ! -L ./bin/log ]; then
+  ln -s \`pwd\`/log ./bin/log
+fi
+
 set -x
-mkdir -p build
 cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=\${BUILD_TYPE}
 make -j7
+set +x
+
+if [[ "\${CLEAN}" == "debug" ]]; then
+  cd ..
+  ./run_test.sh
+
+  COVERAGE_FILE=coverage.info
+  REPORT_FOLDER=coverage_report
+  lcov --rc lcov_branch_coverage=1 -c -d build -o \${COVERAGE_FILE}_tmp
+  lcov --rc lcov_branch_coverage=1  -e \${COVERAGE_FILE}_tmp "*src*" -o \${COVERAGE_FILE}
+  genhtml --rc genhtml_branch_coverage=1 \${COVERAGE_FILE} -o \${REPORT_FOLDER}
+  rm -rf \${COVERAGE_FILE}_tmp
+  rm -rf \${COVERAGE_FILE}
+
+  if [[ "\$OSTYPE" == "darwin"* ]]; then
+    open ./\${REPORT_FOLDER}/index.html
+  fi
+fi
 EOF
