913e996329f1e3d18d02d11ecaa97f0731ae08a6
alibenD <aliben.develop@gmail.com>
2020-03-27 00:17:44 +0800

    Fixed auto_install problem

--- install/init.bash
+++ install/init.bash
@@ -5,7 +5,7 @@
 # @file: init.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2019-08-15 14:01:19
-# @last_modified_date: 2019-12-08 20:56:12
+# @last_modified_date: 2020-03-27 00:14:46
 # @brief: TODO
 # @details: TODO
 #---***********************************************---
@@ -16,6 +16,7 @@ CREATED_TIME=`date '+%Y-%m-%d %H:%M:%S'`
 CREATED_YEAR=`date '+%Y'`
 
 #---Shell Command
+source restore_shell_profile.bash
+source ~/.bash_profile
 ../vim/auto_install.sh
 ../fonts/install.sh
-source restore_shell_profile.bash
