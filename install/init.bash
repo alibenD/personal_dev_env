bd84eb4b9a35f8ff5b40f57fc0aadf0515fa6f22
alibenD <aliben.develop@gmail.com>
2020-04-04 14:40:39 +0800

    Added brew install supports

--- install/init.bash
+++ install/init.bash
@@ -5,7 +5,7 @@
 # @file: init.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2019-08-15 14:01:19
-# @last_modified_date: 2020-03-27 00:14:46
+# @last_modified_date: 2020-04-04 14:39:40
 # @brief: TODO
 # @details: TODO
 #---***********************************************---
@@ -16,7 +16,26 @@ CREATED_TIME=`date '+%Y-%m-%d %H:%M:%S'`
 CREATED_YEAR=`date '+%Y'`
 
 #---Shell Command
-source restore_shell_profile.bash
-source ~/.bash_profile
-../vim/auto_install.sh
-../fonts/install.sh
+if [[ $ALIBEN_DEV_ENV ]]; then
+  echo "Already installed personal_dev_env"
+  exit 0;
+fi
+
+if [[ "$(uname)" = "Linux" ]]; then
+  echo "Linux system, default using apt to manage package"
+  echo "Install dependency"
+  /bin/bash install_linux_dependency.sh
+  /bin/bash install_zsh_plugin
+else
+  if command -v brew >/dev/null 2>&1; then 
+    echo "Exists brew, `which brew`"
+  else 
+    echo "No exists brew, installing brew at $HOME/dev/env"
+    /bin/bash brew_install.sh
+  fi
+fi
+
+#source restore_shell_profile.bash
+#source ~/.bash_profile
+#../vim/auto_install.sh
+#../fonts/install.sh
