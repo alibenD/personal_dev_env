277369afcde628afa59eea148e97877d2baaf78c
alibenD <aliben.develop@gmail.com>
2020-04-04 21:42:00 +0800

    Fixed vim 8.x params for configure

--- install/install_vim.sh
+++ install/install_vim.sh
@@ -4,7 +4,7 @@
 # @file: install_vim.sh
 # @author: aliben.develop@gmail.com
 # @created_date: 2020-04-04 15:46:49
-# @last_modified_date: 2020-04-04 21:30:18
+# @last_modified_date: 2020-04-04 21:41:29
 # @brief: TODO
 # @details: TODO
 #---***********************************************---
@@ -16,23 +16,20 @@ CREATED_YEAR=`date '+%Y'`
 
 #---Shell Command
 mkdir -p $HOME/tmp
-VIM_DOWNLOAD_PATH=$HOME/tmp/vim
-if [ ! -d $VIM_DOWNLOAD_PATH ]; then
+VIM_DOWNLOAD_PATH=$HOME/tmp
+if [ ! -f $VIM_DOWNLOAD_PATH/vim-master.zip ]; then
   #git clone https://github.com/vim/vim.git $VIM_DOWNLOAD_PATH
   #wget https://codeload.github.com/vim/vim/zip/master
-  axel -n 8 -o $VIM_DOWNLOAD_PATH https://codeload.github.com/vim/vim/zip/master
+  axel -n 8 -o $VIM_DOWNLOAD_PATH/vim-master.zip https://codeload.github.com/vim/vim/zip/master
 fi
+
 cd $VIM_DOWNLOAD_PATH
 unzip vim-master.zip
 cd vim-master
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=`ls /usr/lib/python2.7 | grep config-` \ # pay attention here check directory correct
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
-            --with-python3-command=`which python3` \ 
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-gui=gtk2 \
