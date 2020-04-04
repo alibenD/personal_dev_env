122c43f3404ece60c74be9085cfb4919db3c875c
alibenD <aliben.develop@gmail.com>
2020-04-04 22:15:36 +0800

    Added fzf

--- install/install_fzf.sh
+++ install/install_fzf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #---Generated from template 'sh' wrote by @aliben---
 # @Copyright (C) 2020 All rights reserved.
-# @file: brew_package_install.sh
+# @file: install_fzf.sh
 # @author: aliben.develop@gmail.com
-# @created_date: 2020-04-04 16:06:39
-# @last_modified_date: 2020-04-04 21:29:06
+# @created_date: 2020-04-04 22:10:40
+# @last_modified_date: 2020-04-04 22:13:11
 # @brief: TODO
 # @details: TODO
 #---***********************************************---
@@ -15,8 +15,10 @@ CREATED_TIME=`date '+%Y-%m-%d %H:%M:%S'`
 CREATED_YEAR=`date '+%Y'`
 
 #---Shell Command
-#brew install vim --with-python --with-ruby --with-perl --with-features=huge --with-lua
-brew install cmake \
-  wget \
-  vim \
-  axel
+if [[ "$(uname)" = "Linux" ]]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+else
+  brew install fzf
+  $(brew --prefix)/opt/fzf/install
+fi
